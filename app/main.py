3f70c5469cbe206e290f5541c23d57ea69e50035
Jesús Vela <145408906+CUCHO20@users.noreply.github.com>
2025-10-20 00:03:30 -0500

    UPDATED FOR VERCEL DEPLOY

--- app/main.py
+++ app/main.py
@@ -9,3 +9,7 @@ app.include_router(sensor.router)
 @app.get("/")
 async def root():
     return {"message": "Welcome to PulseVitalAPI"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=False)
